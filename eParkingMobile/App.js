c2413555d8098cdc7806b97c0d28dd8839c9507a
marteoma <marteomasoyyo@gmail.com>
2018-11-03 16:42:48 -0500

    Actualización de gitignore para java

--- eParkingMobile/App.js
+++ eParkingMobile/App.js
@@ -21,7 +21,7 @@ export default class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>I changed this message</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.instructions}>I already edited</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );

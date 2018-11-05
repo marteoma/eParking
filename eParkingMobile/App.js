9865dca88565abf4f3d41c5dbde127931d1735d6
marteoma <marteomasoyyo@gmail.com>
2018-11-04 22:30:49 -0500

    Conexión con api

--- eParkingMobile/App.js
+++ eParkingMobile/App.js
@@ -7,22 +7,13 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>I changed this message</Text>
-        <Text style={styles.instructions}>I already edited</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
---
+++ eParkingMobile/src/components/createZona.js
@@ -0,0 +1,15 @@
+// import React, { Component } from "react"
+// import { Text, View } from "react-native"
+// import { apiUrl } from "../api/constants"
+// import axios from "axios"
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       axios.get(`${apiUrl}/zona`)
+//       .then(res => {
+//         <Text>HOLA</Text>
+//       })
+//     )
+//   }
+// }

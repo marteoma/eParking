8b2b7fae96e4868d70f6d8b8b37e780a160c696d
marteoma <marteomasoyyo@gmail.com>
2018-11-24 22:15:00 -0500

    Se muestra la ubicación actual

--- eParkingMobile/src/components/ZonasView.js
+++ eParkingMobile/src/components/ZonasView.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
-import { View, ActivityIndicator, StyleSheet } from "react-native";
-import { MapView } from "expo";
+import { View, ActivityIndicator, StyleSheet, Platform } from "react-native";
+import { MapView, Location, Permissions, Constants, Marker } from "expo";
 import constantes from "../api/constants";
 import axios from "axios";
 
 export default class ZonasView extends Component {
   constructor(props) {
     super(props);
-    this.state = { isLoading: true };
+    this.state = {
+      isLoading: true,
+      location: {
+        latitude: 6.246219,
+        longitude: -75.573044
+      }
+    };
   }
 
   componentDidMount() {
+    this.getCurrentPos();
     this.fetchMarkerData();
   }
 
@@ -18,10 +25,22 @@ export default class ZonasView extends Component {
     title: "Zonas"
   };
 
+  getCurrentPos() {
+    if (Platform.OS === "android" && !Constants.isDevice) {
+      this.setState({
+        errorMessage: "La localización no funciona en un emulador"
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
   fetchMarkerData() {
     return axios
       .get(`${constantes.apiUrl}/zona/all`)
       .then(response => {
+        let markers = response.data;
+        markers.push();
         this.setState({
           isLoading: false,
           markers: response.data
@@ -35,6 +54,23 @@ export default class ZonasView extends Component {
   onPress(zona) {
     this.props.navigation.navigate("Celdas", { zona });
   }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== "granted") {
+      this.setState({
+        errorMessage: "Permiso denegado a la ubicación"
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({
+      location: {
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude
+      }
+    });
+  };
 
   render() {
     if (this.state.isLoading) {
@@ -55,24 +91,33 @@ export default class ZonasView extends Component {
           longitudeDelta: 0.0421
         }}
       >
-        {this.state.isLoading
-          ? null
-          : this.state.markers.map((marker, index) => {
-              const coords = {
-                latitude: marker.lat,
-                longitude: marker.lon
-              };
+        <View>
+          {this.state.isLoading
+            ? null
+            : this.state.markers.map((marker, index) => {
+                const coords = {
+                  latitude: marker.lat,
+                  longitude: marker.lon
+                };
 
-              return (
-                <MapView.Marker
-                  key={index}
-                  coordinate={coords}
-                  title={marker.nombre.toUpperCase().replace(/_/g, " ")}
-                  description={marker.ubicacion}
-                  onPress={() => this.onPress(marker.nombre)}
-                />
-              );
-            })}
+                return (
+                  <MapView.Marker
+                    key={index}
+                    coordinate={coords}
+                    title={marker.nombre.toUpperCase().replace(/_/g, " ")}
+                    description={marker.ubicacion}
+                    onPress={() => this.onPress(marker.nombre)}
+                  />
+                );
+              })}
+          <MapView.Marker
+            key={"house"}
+            coordinate={this.state.location}
+            title="Posición Actual"
+            description=""
+            image={require("../../assets/you.png")}
+          />
+        </View>
       </MapView>
     );
   }
